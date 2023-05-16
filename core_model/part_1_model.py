b0b9628d9591d0ef89fb49773c634bf476b58c5a
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2023-05-17 00:27:29 +0700

    Fixed part 1 model

--- core_model/part_1_model.py
+++ core_model/part_1_model.py
@@ -14,24 +14,24 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-class SimpleCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+# class SimpleCNN(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, 10)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+#     def forward(self, x):
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = torch.flatten(x, 1) # flatten all dimensions except batch
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
     
 """
 TODO: Build your first Custom CNN model
@@ -40,28 +40,30 @@ output: 3
 CNN Architecture:
 
 """
-# define class object model derived from nn.Module
 class YourOwnCNN(nn.Module):
-    # input
-    # define convolutional layer 1
-    # define pooling layer 1
-    # define convolutional layer 2
-    # define pooling layer 2
-    # non-linear activation function
-    # passing to forward function
     def __init__(self):
-        super(YourOwnCNN).__init__()
-        #define the desired architecture
-        pass
-        
-        
+        super(YourOwnCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(16 * 29 * 29, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 3)
+
     def forward(self, x):
-        pass
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
     
 
 ## tester to check if the model is working
-if __name__ =='__main__':
-    model = SimpleCNN()
+if __name__ == '__main__':
+    model = YourOwnCNN()
     print(model)
     x = torch.randn(1, 3, 128, 128)
     print(model(x))
