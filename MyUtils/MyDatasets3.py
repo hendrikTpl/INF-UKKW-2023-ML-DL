dbbaa09e784157f7b7c72074a961929331ca9794
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2023-05-19 03:57:37 +0700

    #HW1 Submission

--- MyUtils/MyDatasets3.py
+++ MyUtils/MyDatasets3.py
@@ -1,11 +1,22 @@
+# /**
+#  * @author Hendrik
+#  * @email hendrik.gian@gmail.com
+#  * @create date 2023-03-16 13:42:16
+#  * @modify date 2023-03-16 13:42:16
+#  * @desc [description]
+#  */
+
 import numpy as np
 import os
 from PIL import Image
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torchvision.utils import save_image
 import torchvision.transforms as transforms
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+import torchvision.transforms as T
+
 
 both_transform = A.Compose(
     [A.Resize(width=512, height=512),], additional_targets={"image0": "image"},
@@ -15,23 +26,41 @@ transform_only_input = A.Compose(
     [
         A.HorizontalFlip(p=0.5),
         A.ColorJitter(p=0.2),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[
+                    0.5, 0.5, 0.5], max_pixel_value=255.0,),
         ToTensorV2(),
     ]
 )
 
 transform_only_mask = A.Compose(
     [
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255.0,),
+        A.Normalize(mean=[0.5, 0.5, 0.5], std=[
+                    0.5, 0.5, 0.5], max_pixel_value=255.0,),
         ToTensorV2(),
     ]
 )
 
+# TODO: sesuaikan dataset UKRIDA dataset, indonesian food street, 3 label, bakso, mie ayam, batagor
+# indonesian street food
+
+# non-aligned data 256x256x3
+
+
+class UkridaDataset_v1(Dataset):
+    pass
+    # folder base data loader
+    # metadata.csv data loader
+
+
+# Pix2Pix Dataloader from folder
+# A, B AB -> aligned data
 class UkridaDataset(Dataset):
     def __init__(self, root_dir):
         self.root_dir = root_dir
         self.list_files = os.listdir(self.root_dir)
+        # make sure all the listed files are in FILE_EXT
         self.list_files = [i for i in self.list_files if i.endswith(".png")]
+        # print(self.list_files)
 
     def __len__(self):
         return len(self.list_files)
@@ -40,12 +69,13 @@ class UkridaDataset(Dataset):
         img_file = self.list_files[index]
         img_path = os.path.join(self.root_dir, img_file)
         image = np.array(Image.open(img_path))
-        image = image[:, :, :3]  # Convert RGBA to RGB
-
-        w_img = image.shape[1]
-        w_img_half = w_img // 2
+        w_img = image.shape[1]  # get the width size of image
+        w_img_half = w_img // 2  # assuming dataset img is aligned x,y
+        # since input and target images were aligned side by side, divided into two 512X512
         input_image = image[:, :w_img_half, :]
         target_image = image[:, w_img_half:, :]
+        # input_image = image[:, :512, :]
+        # target_image = image[:, 512:, :]
 
         augmentations = both_transform(image=input_image, image0=target_image)
         input_image = augmentations["image"]
@@ -57,26 +87,13 @@ class UkridaDataset(Dataset):
         return input_image, target_image
 
 
-class CustomDataLoader:
-    def __init__(self, root_dir, batch_size=1, shuffle=True):
-        self.dataset = UkridaDataset(root_dir)
-        self.dataloader = DataLoader(
-            self.dataset,
-            batch_size=batch_size,
-            shuffle=shuffle
-        )
-
-    def __iter__(self):
-        return iter(self.dataloader)
-
-    def __len__(self):
-        return len(self.dataloader)
-
-
 # test case
 if __name__ == "__main__":
-    dataloader = CustomDataLoader("C:/Users/Asus/ML-2023/INF-UKKW-2023-ML-DL/data/IndonesianStreetFood/train", batch_size=1, shuffle=True)
-    for x, y in dataloader:
+    dataset = UkridaDataset("./data/IndonesianStreetFood/train")
+    loader = DataLoader(dataset)
+    for x, y in loader:
         print(x.shape)
+        # save_image(x, "x.png")
+        # save_image(y, "y.png")
         import sys
         sys.exit()
